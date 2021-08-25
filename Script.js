9e88f5279a2985ab6c4f9c58e60605515ab4d6f6
Weitix <kirill2000gubin@gmail.com>
2021-08-25 17:21:01 +0300

    fix github page upd

--- Script.js
+++ Script.js
@@ -1,3 +1,78 @@
+//scroll докрутка до секции 
+var myFullpage = new fullpage('#fullpage', {
+  //Navigation
+  menu: '#menu',
+  lockAnchors: false,
+  anchors:['.slide1', 'slide2','slide3','slide4','slide5'],
+  //Scrolling
+  css3: true,
+  scrollingSpeed: 1500,
+  autoScrolling: false,
+  fitToSection: true,
+  fitToSectionDelay: 1000,
+  scrollBar: false,
+  easing: 'easeInOutCubic',
+  easingcss3: 'ease',
+  loopBottom: false,
+  loopTop: false,
+  loopHorizontal: true,
+  continuousVertical: false,
+  continuousHorizontal: false,
+  scrollHorizontally: false,
+  interlockedSlides: false,
+  dragAndMove: false,
+  offsetSections: false,
+  resetSliders: false,
+  fadingEffect: false,
+  scrollOverflow: false,
+  scrollOverflowReset: false,
+  scrollOverflowOptions: null,
+  touchSensitivity: 15,
+  bigSectionsDestination: null,
+  
+  //Accessibility
+  
+  
+  //Design
+  /*controlArrows: true,
+  verticalCentered: true,
+  sectionsColor : ['#ccc', '#fff'],
+  paddingTop: '3em',
+  paddingBottom: '10px',
+  fixedElements: '#header, .footer',
+  responsiveWidth: 0,
+  responsiveHeight: 0,
+  responsiveSlides: false,
+  parallax: false,
+  parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+  dropEffect: false,
+  dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
+  waterEffect: false,
+  waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
+  cards: false,
+  cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},*/
+  
+  //Custom selectors
+  sectionSelector: '.section',
+  slideSelector: '.slide',
+  
+  lazyLoading: true,
+  
+  //events
+  onLeave: function(origin, destination, direction){},
+  afterLoad: function(origin, destination, direction){},
+  afterRender: function(){},
+  afterResize: function(width, height){},
+  afterReBuild: function(){},
+  afterResponsive: function(isResponsive){},
+  afterSlideLoad: function(section, origin, destination, direction){},
+  onSlideLeave: function(section, origin, destination, direction){}
+  });
+
+
+
+
+
 
 //обработчик события 
 const Boxess = document.querySelectorAll('.light');
@@ -86,76 +161,7 @@ box.classList.remove('show','bgoff');
         navigation.classList.toggle('active');
     }
 
-//scroll докрутка до секции 
-var myFullpage = new fullpage('#fullpage', {
-//Navigation
-menu: '#menu',
-lockAnchors: false,
-anchors:['.slide1', 'slide2','slide3','slide4','slide5'],
-//Scrolling
-css3: true,
-scrollingSpeed: 1500,
-autoScrolling: false,
-fitToSection: true,
-fitToSectionDelay: 1000,
-scrollBar: false,
-easing: 'easeInOutCubic',
-easingcss3: 'ease',
-loopBottom: false,
-loopTop: false,
-loopHorizontal: true,
-continuousVertical: false,
-continuousHorizontal: false,
-scrollHorizontally: false,
-interlockedSlides: false,
-dragAndMove: false,
-offsetSections: false,
-resetSliders: false,
-fadingEffect: false,
-scrollOverflow: false,
-scrollOverflowReset: false,
-scrollOverflowOptions: null,
-touchSensitivity: 15,
-bigSectionsDestination: null,
 
-//Accessibility
-
-
-//Design
-/*controlArrows: true,
-verticalCentered: true,
-sectionsColor : ['#ccc', '#fff'],
-paddingTop: '3em',
-paddingBottom: '10px',
-fixedElements: '#header, .footer',
-responsiveWidth: 0,
-responsiveHeight: 0,
-responsiveSlides: false,
-parallax: false,
-parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-dropEffect: false,
-dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
-waterEffect: false,
-waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
-cards: false,
-cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},*/
-
-//Custom selectors
-sectionSelector: '.section',
-slideSelector: '.slide',
-
-lazyLoading: true,
-
-//events
-onLeave: function(origin, destination, direction){},
-afterLoad: function(origin, destination, direction){},
-afterRender: function(){},
-afterResize: function(width, height){},
-afterReBuild: function(){},
-afterResponsive: function(isResponsive){},
-afterSlideLoad: function(section, origin, destination, direction){},
-onSlideLeave: function(section, origin, destination, direction){}
-});
 
 //неоновые буквы 
 function neos() {$(document).ready(function () {
